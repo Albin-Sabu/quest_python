c780bd900351be1fdb60d82fedd2aae08ecb6210
Albin-Sabu <albinsabualpt@gmail.com>
2024-08-07 14:22:53 +0530

    assignment submitted

--- day7/assignment.py
+++ day7/assignment.py
@@ -4,6 +4,8 @@ age = int(input("Enter age: "))
 gender = input("Enter gender: \nM. Male\nF. Female\n")
 occupation = input("Enter occupation: \nS. Student \nW. working\n")
 residence = input("Enter Residence: \nH. Hosteler \nL. Localite\n")
+residencePin = int(input("Enter your pin:"))
+pin = [100011, 100234, 100254, 100001]
 #########################################################################
 
 
@@ -17,10 +19,16 @@ elif age < 45 and gender.lower() == "f" and residence.lower() == "h" and occupat
     print("Grocery discount and fastrack/nyka coupon added, thank you for shopping")
     
 elif age < 45 and gender.lower() == "f" and residence.lower() == "h" and occupation.lower() == "s":
-    print("Grocery discount and fastrack/nyka coupon added and a book discount coupon added, thank you for shopping")
+    if residencePin in pin:
+        print("Grocery discount and fastrack/nyka coupon added and a book discount coupon added, thank you for shopping")
+    else:
+        print("Grocery discount and fastrack/nyka coupon added, thank you for shopping")
 
 elif age < 60 and gender.lower() == "m" and residence.lower() == "h" and occupation.lower() == "s":
-    print("Grocery discount and fastrack/titan coupon added and a discount for book is also added, thank you for shopping")
+    if residencePin in pin:
+        print("Grocery discount and fastrack/titan coupon added and a discount for book is also added, thank you for shopping")
+    else:
+        print("Grocery discount and fastrack/Titan coupon added , thank you for shopping")
 
 elif age < 60 and gender.lower() == "m" and residence.lower() == "h" and occupation.lower() == "w":
     print("Grocery discount and fastrack/titan coupon added, , thank you for shopping")
@@ -29,13 +37,20 @@ elif age < 45 and gender.lower() == "f" and residence.lower() == "l" and occupat
     print("Fastrack/nyka coupon added, thank you for shopping")
 
 elif age < 45 and gender.lower() == "f" and residence.lower() == "l" and occupation.lower() == "s":
-    print("fastrack/titan coupon added book discount also added, , thank you for shopping")
+    if residencePin in pin:
+        print("fastrack/nyka coupon added book discount also added, , thank you for shopping")
+    else:
+        print("Fastrack/nyka coupon added , thank you for shopping")
+    
 
 elif age < 60 and gender.lower() == "m" and residence.lower() == "l" and occupation.lower() == "w":
     print("Fastrack/Titan coupon added")
 
 elif age < 60 and gender.lower() == "m" and residence.lower() == "l" and occupation.lower() == "s":
-    print("Fastrack/Titan coupon added,  book dicount also added, thank you for shopping")
+    if residencePin in pin:
+        print("fastrack/Titan coupon added book discount also added, , thank you for shopping")
+    else:
+        print("Fastrack/Titan coupon added , thank you for shopping")
 
         
 else:
